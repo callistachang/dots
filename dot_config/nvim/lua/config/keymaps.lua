e1e4b8171ceff80d4d36f326f580054969e95535
Callista Chang <changcallista@gmail.com>
2023-01-29 21:46:41 +0800

    fixes

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -10,8 +10,13 @@ vim.keymap.set("n", ",w", "<cmd>w<cr>")
 vim.keymap.set("i", ",w", "<esc><cmd>w<cr>")
 
 -- Quick quit
-vim.keymap.set("n", ",q", "<cmd>qa<cr>")
-vim.keymap.set("i", ",q", "<esc><cmd>qa<cr>")
+vim.keymap.set("n", ",q", "<cmd>q<cr>")
+vim.keymap.set("i", ",q", "<esc><cmd>q<cr>")
+vim.keymap.set("n", ",x", "<cmd>qa<cr>")
+vim.keymap.set("i", ",x", "<esc><cmd>qa<cr>")
+
+-- Quick format
+vim.keymap.set("n", ",f", vim.lsp.buf.format)
 
 -- Source vim/nvim file
 vim.keymap.set("n", ",s", "<cmd>so %<cr>", { desc = "Source vim/nvim file" })
@@ -39,12 +44,10 @@ vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Go to upper window" })
 vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Go to right window" })
 
 -- Shift lines up and down
-vim.keymap.set("n", ",j", ":m .+1<cr>==", { desc = "Move down" })
-vim.keymap.set("v", ",j", ":m '>+1<cr>gv=gv", { desc = "Move down" })
-vim.keymap.set("i", ",j", "<Esc>:m .+1<cr>==gi", { desc = "Move down" })
-vim.keymap.set("n", ",k", ":m .-2<cr>==", { desc = "Move up" })
-vim.keymap.set("v", ",k", ":m '<-2<cr>gv=gv", { desc = "Move up" })
-vim.keymap.set("i", ",k", "<Esc>:m .-2<cr>==gi", { desc = "Move up" })
+vim.keymap.set("n", "J", ":m .+1<cr>==", { desc = "Move down" })
+vim.keymap.set("v", "J", ":m '>+1<cr>gv=gv", { desc = "Move down" })
+vim.keymap.set("n", "K", ":m .-2<cr>==", { desc = "Move up" })
+vim.keymap.set("v", "K", ":m '<-2<cr>gv=gv", { desc = "Move up" })
 
 -- Start and end of line
 vim.keymap.set("n", ",h", "^")
@@ -52,3 +55,15 @@ vim.keymap.set("n", ",l", "g_")
 
 -- u to undo, so U to redo
 vim.keymap.set("n", "U", "<c-r>")
+
+-- Code runner
+vim.keymap.set("n", ",r", "<cmd>RunCode<cr>", { noremap = true, silent = false })
+
+-- Floating terminal
+vim.keymap.set("n", ",t", "<cmd>ToggleTerm<cr>")
+vim.keymap.set("t", "<esc>", [[<c-\><c-n>]])
+vim.keymap.set("t", "<C-h>", [[<c-\><c-n><C-w>h]], { desc = "Go to left window" })
+vim.keymap.set("t", "<C-j>", [[<c-\><c-n><C-w>j]], { desc = "Go to lower window" })
+vim.keymap.set("t", "<C-k>", [[<c-\><c-n><C-w>k]], { desc = "Go to upper window" })
+vim.keymap.set("t", "<C-l>", [[<c-\><c-n><C-w>l]], { desc = "Go to right window" })
+vim.keymap.set({ "t", "n" }, "<esc><esc>", [[<c-\><c-n><cmd>ToggleTerm<cr>]])
