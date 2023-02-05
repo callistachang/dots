ce9f6ec55c907c7d0b3a8c6d1fb1af94b92416dd
Callista Chang <changcallista@gmail.com>
2023-02-05 17:43:35 +0800

    nvim: add vimspector configs

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -1,3 +1,47 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+vim.g.vimspector_enable_mappings = "HUMAN"
+
+vim.g.vimspector_install_gadgets = { 'debugpy', 'codelldb' }
+
+vim.g.vimspector_configurations = {
+  python = {
+    adapter = "debugpy",
+    configuration = {
+      python = "python",
+      request = "launch",
+      type = "python",
+      program = "${file}",
+      stopOnEntry = false,
+      cwd = "${workspaceRoot}",
+      console = "integratedTerminal"
+    },
+    breakpoints = {
+      exception = {
+        raised = "N",
+        uncaught = "",
+        userUnhandled = "",
+      }
+    }
+  },
+  cpp = {
+    adapter = "CodeLLDB",
+    configuration = {
+      type = "cpp",
+      request = "launch",
+      program = "${file}",
+      stopOnEntry = false,
+      cwd = "${workspaceRoot}",
+      console = "integratedTerminal"
+    },
+    breakpoints = {
+      exception = {
+        raised = "N",
+        uncaught = "",
+        userUnhandled = "",
+      }
+    }
+  }
+}
